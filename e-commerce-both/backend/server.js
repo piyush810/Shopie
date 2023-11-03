091a62df001ccc427729bbea63d63b0f283a7e77
Piyush Kumar <yopiyush8100@gmail.com>
2023-11-03 11:51:09 +0530

    updated commit

--- e-commerce-both/backend/server.js
+++ e-commerce-both/backend/server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import cors from "cors";
+import path from 'path';
 
 //configure env
 dotenv.config();
@@ -22,6 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -29,9 +31,13 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order",orderRoutes);
 
+
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Shopie</h1>");
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to Shopie</h1>");
+// });
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 
 //PORT
